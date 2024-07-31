d7405441e348c2c716337525fba0b56a2ebd6969
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2024-07-31 07:20:56 +0300

    00018 use CenterComponent in your app as custom widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import { ArcgisMap } from "@arcgis/map-components-react";
+import { CenterComponent } from "./components/CenterComponent/CenterComponent";
+
 import "./App.css";
 
 function App() {
+  const centerComponentID = "center-component";
+
   return (
-    <div className="mapDiv">
-      <ArcgisMap center={[55, 25]} zoom={13}></ArcgisMap>
-    </div>
+    <>
+      <div className="mapDiv">
+        <ArcgisMap
+          center={[55, 25]}
+          zoom={13}
+          onArcgisViewReadyChange={(event) => {
+            const { view }: { view: __esri.MapView } = event.target;
+            view.ui.add(centerComponentID, "bottom-right");
+          }}
+        ></ArcgisMap>
+      </div>
+      <CenterComponent id={centerComponentID} />
+    </>
   );
 }
 
